aa2409420e5f2accd9b37b9c0dde917f4bac5ca1
Harsh <harshb15003@gmail.com>
2024-04-12 13:04:27 +0530

    let's do it

---
+++ client/src/pages/HomeOnline.jsx
@@ -0,0 +1,14 @@
+import React  from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const HomeOnline = () => {
+
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default HomeOnline
