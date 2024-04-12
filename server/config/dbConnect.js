00fbf0994a51c17e0b25fcd9efe219144180d991
Akshat Gupta <akshatgtc@gmail.com>
2024-04-13 02:19:39 +0530

    donr

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://akshatgtc:akshat@cluster0.8gjvxnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{console.log("MongoDB Connected")});
+    const conn = await mongoose.connect("mongodb+srv://akshatgtc:akshat@cluster0.hxbjqwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{console.log("MongoDB Connected")});
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
