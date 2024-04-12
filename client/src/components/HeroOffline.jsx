e1f08e637434d8259d662cabdc76b2f9503d3ed5
Akshat Gupta <akshatgtc@gmail.com>
2024-04-13 03:48:39 +0530

    done

--- client/src/components/HeroOffline.jsx
+++ client/src/components/HeroOffline.jsx
@@ -46,7 +46,7 @@ const HeroOffline = () => {
           <span>Check Balance</span>
         </button>
       </Link>
-      <Link to='/payUpi' className="col-span-1 flex flex-col items-center justify-center space-y-2">
+      <Link to='/payUpiOffline' className="col-span-1 flex flex-col items-center justify-center space-y-2">
         {/* Icon 5 */}
         <button className='flex flex-col items-center justify-center bg-[#fdf8f8] shadow-lg rounded-lg pt-4 pb-3 pl-2 pr-2'>
           <TollIcon />
