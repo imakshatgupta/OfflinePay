9c61ae07952e2bbb736aaf579cf55c95c7fb23b2
Harsh <harshb15003@gmail.com>
2024-04-13 00:18:39 +0530

    login-signup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,38 @@
+import { useState } from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import './App.css'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import QrCode from './pages/QrCode'
+import QrScanner from './pages/QrScanner'
+import Transaction from './pages/Transaction'
+import CheckBalance from './pages/CheckBalance'
 import HomeOnline from './pages/HomeOnline'
 import HomeOffline from './pages/HomeOffline'
-import Signup from './components/Signup'
-import Login from './components/Login'
-
+import EncryptedData from './pages/EncryptedData'
+import OfflinePay from './pages/OfflinePay'
+import PayUPI from './pages/PayUPI'
+import CheckBalanceOffline from './pages/CheckBalanceOffline'
+import TransactionOffline from './pages/TransactionOffline'
 
 function App() {
-
   return (
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<HomeOnline/>}/>
-        <Route path="/offlinepay" element={<HomeOffline/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path="/" element={<HomeOnline />} />
+        <Route path='/offlinePay' element={<HomeOffline />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/qr' element={<QrCode />} />
+        <Route path='/qrscanner' element={<QrScanner />} />
+        <Route path='/payUpi' element={<PayUPI />} />
+        <Route path="/encrypt" element={<EncryptedData />} />
+        <Route path='/transaction' element={<Transaction />} />
+        <Route path='/transactionOffline' element={<TransactionOffline />} />
+        <Route path='/checkBalance' element={<CheckBalance />} />
+        <Route path="/offline" element={<OfflinePay />} />
+        <Route path="/checkbalanceoffline" element={<CheckBalanceOffline />} />
       </Routes>
     </Router>
     </>
--- client/src/pages/HomeOnline.jsx
+++ client/src/pages/HomeOnline.jsx
@@ -1,10 +1,27 @@
-import React  from 'react'
+import React , {useEffect} from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import DemoCarousel from '../components/Carousel'
+import axios from 'axios'
 import Hero from '../components/HeroOnline'
 
 const HomeOnline = () => {
 
+  useEffect(() => {
+    getUser();
+  },[]);
+
+  const getUser = async () => {
+    const user  = localStorage.getItem("userId");
+    if(!user){
+      window.location.href = "/login";
+    }
+    const res = await axios.get("https://dtu.onrender.com/users/getUser" , {
+      headers: {
+        Authorization: `${localStorage.getItem("userId")}`
+      }
+    });
+    console.log(res.data.user);
+  }
 
   return (
     <>
