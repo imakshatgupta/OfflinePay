0246785e05a1b8cc984f6fc4c89a97e2df2b5f9c
Harsh <harshb15003@gmail.com>
2024-04-12 13:36:38 +0530

    carousel

--- client/src/pages/HomeOnline.jsx
+++ client/src/pages/HomeOnline.jsx
@@ -1,5 +1,6 @@
 import React  from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import DemoCarousel from '../components/Carousel'
 
 const HomeOnline = () => {
 
@@ -7,6 +8,7 @@ const HomeOnline = () => {
   return (
     <>
       <Navbar/>
+      <DemoCarousel/>
     </>
   )
 }
